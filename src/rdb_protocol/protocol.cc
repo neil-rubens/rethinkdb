536d0d3bb863c25f90ea3637b8c738de1f679d4c
Graham Hughes <graham@rethinkdb.com>
2014-07-15 20:07:37 -0700

    Derp, use right macro

--- src/rdb_protocol/protocol.cc
+++ src/rdb_protocol/protocol.cc
@@ -1177,7 +1177,8 @@ RDB_IMPL_SERIALIZABLE_0_SINCE_v1_13(sindex_list_t);
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(sindex_status_t, sindexes, region);
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(changefeed_subscribe_t, addr, region);
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(changefeed_stamp_t, addr, region);
-RDB_IMPL_SERIALIZABLE_2(read_t, read, profile, limits);
+
+RDB_IMPL_SERIALIZABLE_3(read_t, read, profile, limits);
 
 RDB_IMPL_SERIALIZABLE_1_SINCE_v1_13(point_write_response_t, result);
 RDB_IMPL_SERIALIZABLE_1_SINCE_v1_13(point_delete_response_t, result);
@@ -1201,7 +1202,7 @@ RDB_IMPL_SERIALIZABLE_4_SINCE_v1_13(sindex_create_t, id, mapping, region, multi)
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(sindex_drop_t, id, region);
 RDB_IMPL_SERIALIZABLE_1_SINCE_v1_13(sync_t, region);
 
-RDB_IMPL_SERIALIZABLE_3(
+RDB_IMPL_SERIALIZABLE_4(
     write_t, write, durability_requirement, profile, limits);
 
 // Serialization format changed in 1.13.2. We only support the latest version,
